68454980d00a2ffb72ba2b7cfcc3da2a14ccab5a
hafijur-rahman1 <hafijur.info24@gmail.com>
2023-12-19 02:23:50 +0600

    header added

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,11 @@ import Generator from "./Generator";
 function App() {
   return (
     <>
-      <div className=" bg-gray-900 w-full h-screen py-5">
+      <div className="relative bg-gray-900 w-full h-screen py-5">
         <Generator />
+        <div className="mx-auto top-1/2 left-1/2 absolute -translate-x-1/2 -translate-y-1/2 text-zinc-500 text-[42px] font-semibold italic">
+          Password Generator
+        </div>
       </div>
     </>
   );
